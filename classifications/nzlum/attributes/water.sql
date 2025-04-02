f873953da41912cd2288beb38fdfad1c4980f98f
alpha-beta-soup <lawr@landcareresearch.co.nz>
2025-04-02 14:04:51 +1300

    add missing chathams topo50 data

--- classifications/nzlum/attributes/water.sql
+++ classifications/nzlum/attributes/water.sql
@@ -4,6 +4,7 @@ CREATE TEMPORARY VIEW water_features AS (
         SELECT h3_index,
         COALESCE(
             rivers.feature,
+            rivers_polylines.feature,
             ecan_braided_rivers_.water,
 
             fenz_lakes_.water,
@@ -34,7 +35,7 @@ CREATE TEMPORARY VIEW water_features AS (
             END
         ) AS feature
         FROM roi
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'river' AS feature
             FROM topo50_river_pol
             RIGHT JOIN topo50_river_pol_h3 USING (ogc_fid)
@@ -44,37 +45,59 @@ CREATE TEMPORARY VIEW water_features AS (
             RIGHT JOIN topo50_chatham_river_pol_h3 USING (ogc_fid)
         ) rivers
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
+            SELECT *, 'river' AS feature
+            FROM topo50_river
+            RIGHT JOIN topo50_river_h3 USING (ogc_fid)
+            UNION ALL
+            SELECT *, 'river' AS feature
+            FROM topo50_chatham_river
+            RIGHT JOIN topo50_chatham_river_h3 USING (ogc_fid)
+        ) rivers_polylines
+        USING (h3_index)
+        LEFT JOIN (
             SELECT *, 'lake' AS feature
             FROM topo50_lake
             RIGHT JOIN topo50_lake_h3 USING (ogc_fid)
+            UNION ALL
+            SELECT *, 'lake' AS feature
+            FROM topo50_chatham_lake
+            RIGHT JOIN topo50_chatham_lake_h3 USING (ogc_fid)
         ) lakes -- NB lake_use records 'hydro-electric' and 'reservoir'
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'lagoon' AS feature
             FROM topo50_lagoon
             RIGHT JOIN topo50_lagoon_h3 USING (ogc_fid)
+            UNION ALL
+            SELECT *, 'lagoon' AS feature
+            FROM topo50_chatham_lagoon
+            RIGHT JOIN topo50_chatham_lagoon_h3 USING (ogc_fid)
         ) lagoons
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'pond' AS feature
             FROM topo50_pond
             RIGHT JOIN topo50_pond_h3 USING (ogc_fid)
         ) ponds
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'swamp' AS feature
             FROM topo50_swamp
             RIGHT JOIN topo50_swamp_h3 USING (ogc_fid)
+            UNION ALL
+            SELECT *, 'swamp' AS feature
+            FROM topo50_chatham_swamp
+            RIGHT JOIN topo50_chatham_swamp_h3 USING (ogc_fid)
         ) swamps
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'mangrove' AS feature
             FROM topo50_mangrove
             RIGHT JOIN topo50_mangrove_h3 USING (ogc_fid)
         ) mangroves
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'drain' AS feature
             FROM topo50_drain
             RIGHT JOIN topo50_drain_h3 USING (ogc_fid)
@@ -84,19 +107,19 @@ CREATE TEMPORARY VIEW water_features AS (
             RIGHT JOIN topo50_chatham_drain_h3 USING (ogc_fid)
         ) drains
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'canal' AS feature
             FROM topo50_canal
             RIGHT JOIN topo50_canal_h3 USING (ogc_fid)
         ) canals
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'ice' AS feature
             FROM topo50_ice
             RIGHT JOIN topo50_ice_h3 USING (ogc_fid)
         ) ice
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *, 'mud' AS feature
             FROM topo50_mud
             RIGHT JOIN topo50_mud_h3 USING (ogc_fid)
@@ -106,9 +129,9 @@ CREATE TEMPORARY VIEW water_features AS (
             RIGHT JOIN topo50_chatham_mud_h3 USING (ogc_fid)
         ) mud
         USING (h3_index)
-        FULL OUTER JOIN topo50_land_h3 AS land
+        LEFT JOIN topo50_land_h3 AS land
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT *
             FROM topo50_sand_h3
             UNION ALL
@@ -116,13 +139,13 @@ CREATE TEMPORARY VIEW water_features AS (
             FROM topo50_chatham_sand_h3
         ) AS sand
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT h3_index, water
             FROM ecan_braided_rivers_h3
             JOIN ecan_braided_rivers USING (ogc_fid)
         ) AS ecan_braided_rivers_
         USING (h3_index)
-        FULL OUTER JOIN (
+        LEFT JOIN (
             SELECT h3_index, water
             FROM fenz_lakes_h3
             JOIN fenz_lakes USING (ogc_fid)
